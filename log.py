185435db2a1781b51859d9bffff65d3f2bbedc75
Suhaib Umair <154515857+MuhammedSuhaib@users.noreply.github.com>
2025-09-10 15:17:43 +0500

    reality check

--- log.py
+++ log.py
@@ -1,6 +1,5 @@
-from agents import Agent, Runner,enable_verbose_stdout_logging
+from agents import Agent, Runner
 from play_ground.config import model_config
-enable_verbose_stdout_logging()
-agent: Agent = Agent(name=1,instructions="You are a helpful assistant.",model=model_config)
-result: Runner = Runner.run_sync(agent, "reply in short")
+agent: Agent = Agent(name='agent',instructions="You are a helpful assistant.",model=model_config,)
+result: Runner = Runner.run_sync(agent, "reply in short",max_turns=1)
 print(result.final_output)
